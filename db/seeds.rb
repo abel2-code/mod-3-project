22f722fba11740b858368e41a2e6f8aab3b99da0
MarkDLR <mark.p.dlr@gmail.com>
2020-07-24 13:52:36 -0500

    added test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Customer.destroy_all
+Company.destroy_all
+Product.destroy_all
+Ad.destroy_all
+Post.destroy_all
+
+user = Customer.create(username: "Mark1", email: "mpd@gmail.com", password: "password", address: "123 Fake Street" )
+company = Company.create(name: "Google", email: "google@gmail.com", password: "password1")
+product = Product.create(name: "shirt1", image: "insert_url", price: 20, company_id: 1)
+post = Post.create(content: "This company is good, products are great", customer_id: 1, company_id: 1)
+ad = Ad.create(company_id: 1)
+
